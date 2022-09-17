b956a6cf8e440ae6749d950312a7f778fef60861
leenchan <leenchan@gmail.com>
2022-09-17 04:19:28 +0000

    preset ip address to 192.168.100.1

--- target/linux/realtek/base-files.mk
+++ target/linux/realtek/base-files.mk
@@ -11,6 +11,13 @@ HOST_LN="$(subst ${STAGING_DIR_HOST},$${STAGING_DIR_HOST},$(LN))"
 
 [ -n "$${IPKG_INSTROOT}" ] && {
 	$${HOST_SED} '/^src\/gz openwrt_core /d' "$${IPKG_INSTROOT}/etc/opkg/distfeeds.conf"
+
+	$${HOST_SED} 's/"192.168.1.1"/"192.168.100.1"/' \
+		"$${IPKG_INSTROOT}/usr/lib/lua/luci/controller/admin/system.lua" \
+		"$${IPKG_INSTROOT}/usr/lib/lua/luci/controller/admin/ota.lua" \
+		"$${IPKG_INSTROOT}/etc/board.d/99-default_network"
+
+	$${HOST_SED} "s/'192\\.168\\.1\\.1'/'192.168.100.1'/; s/'openwrt\\.lan'/window.location.host/" "$${IPKG_INSTROOT}/www/luci-static/resources/view/system/flash.js"
 }
 true
 
